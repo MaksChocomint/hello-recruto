1966590826a0273d9b3efe3f1f91f6e360514bd1
Maksim <maxprofix@gmail.com>
2026-06-15 18:59:17 +0300

    commit

--- app/route.ts
+++ app/route.ts
@@ -3,8 +3,8 @@ import type { NextRequest } from "next/server";
 export function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
 
-  const name = searchParams.get("name") || "Recruto";
-  const message = searchParams.get("message") || "Давай дружить";
+  const name = searchParams.get("name") || "Имя";
+  const message = searchParams.get("message") || "Сообщение";
 
   return new Response(`Hello ${name}! ${message}!`, {
     headers: {
